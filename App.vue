91f5514734bf3230c695a8dff63fd61f9f86784a
Joel Degerman <joel@joeldegerman.se>
2020-05-05 16:56:19 +0200

    Added the structure

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <Header/>
+    <Main/>
+  </section>
+</template>
+
+<script>
+import Header from './Header'
+import Main from './Main'
+// import Footer from './Footer'
+
+export default {
+  components: {
+    Header: Header,
+    Main: Main,
+    // Footer: Footer
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+  * {
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    padding: 0 1rem;
+  }
+</style>
